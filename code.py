ed29a0a1233f8abd583e71dc05acffca8afbfa83
TomWin <miselnicky@gmail.com>
2020-01-15 19:56:52 +0100

    Dalsi

--- code.py
+++ code.py
@@ -1,8 +1,19 @@
 import boto3
+# import sys
+import click
 
 session = boto3.Session(profile_name = 'test')
-
 ec2 = session.resource('ec2')
 
-for item in ec2.instances.all():
-    print(item)
+@click.command()
+def list_instances():
+    for item in ec2.instances.all():
+        print(', '.join((
+            item.id,
+            item.instance_type,
+            item.placement['AvailabilityZone'])))
+    return
+
+if __name__ == '__main__':
+#    print(sys.argv)
+    list_instances()
